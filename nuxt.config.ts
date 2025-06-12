f77c2a8c1f75cb41d5dd7e4abfd3130f9bb3e5a3
Natdanai <natdanai.u@yeeraf.co.th>
2025-06-12 15:00:35 +0700

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   ssr: false,
-  // devtools: { enabled: true },
+  app: {
+    baseURL: '/mtl-irr-calculation/', // e.g., '/my-nuxt-blog/'
+    buildAssetsDir: 'assets', // Prevents GitHub Pages from ignoring folders starting with '_'
+  },
   css: ['~/assets/css/input.css'], // you'll have to create this file
   vite: {
     plugins: [
